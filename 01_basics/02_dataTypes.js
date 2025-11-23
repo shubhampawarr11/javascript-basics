49d5307880f87c8acf33b7e9a17bdb2c9530d5ca
Shubham Pawar <shubhampawar72184@gmail.com>
2025-11-23 07:00:07 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3
+    +
+3) // code readability should be high
+
+console.log("SP");
+
+let name = "SP" // string
+let age = 18; // number
+let isLoggedIn = false // boolean
+let value; // undefined
+let sum = null; // null value means it is empty
+
+// number => 2 to power 53
+// bigInt => Big Values Present
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when you not define the value
+// symbol => to find uniqueness we use symbol
+
+// object
+
+console.log(typeof undefined); // Type: undefined
+console.log(typeof null); // Type: object
