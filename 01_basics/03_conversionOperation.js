5e5a9e7d3f4c076dd3d63a74cc00d5972e424ef6
Shubham Pawar <shubhampawar72184@gmail.com>
2025-11-24 09:13:55 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "99";
 
 console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof (score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
@@ -25,3 +25,38 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// *************** Operations **************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello "
+let str2 = "SP"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(-true);
+
+let gameCounter = 100
+// ++gameCounter;
+gameCounter++;
+console.log(gameCounter);
