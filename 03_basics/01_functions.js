1182d82dcc1383c7ff6c50e322db8d21794f0da4
Shubham Pawar <shubhampawar72184@gmail.com>
2025-12-05 10:31:49 +0000

    Taking Parameters in Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,4 +37,36 @@ function loginUserMessage(username = "SP") {
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("SAM"));
+// console.log(loginUserMessage("SAM"));
+
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+
+const user = {
+    username: "SP",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "SAM",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
