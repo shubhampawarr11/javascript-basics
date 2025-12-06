288bf3c0af21014258ff3821357e3ccdcb3d6298
Shubham Pawar <shubhampawar72184@gmail.com>
2025-12-06 17:33:38 +0000

    Scope this and Arrow

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,6 +8,50 @@ if (true) {
 
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "SDP"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+
+if (true) {
+    const username = "shubham"
+    if (username === "shubham") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ***************** intresting ******************
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(3));
