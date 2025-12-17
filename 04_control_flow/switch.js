1c7db8231c523ac95f319278e4362c5315e8ac4b
Shubham Pawar <shubhampawar72184@gmail.com>
2025-12-17 09:05:41 +0000

    Switch Case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "may"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+    default:
+        console.log("Month not valued");
+        break;
+}
